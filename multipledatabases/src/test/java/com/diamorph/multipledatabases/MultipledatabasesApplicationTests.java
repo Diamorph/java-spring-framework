3ea1395cc3fb3c9081f7fe9d6d4613182b9736fb
diamorph <diamorph1309@gmail.com>
2023-12-08 22:15:35 -0500

    Clinic Project

---
+++ multipledatabases/src/test/java/com/diamorph/multipledatabases/MultipledatabasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diamorph.multipledatabases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipledatabasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
